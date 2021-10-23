fed5d0b24e46d2291b772b63321f90cdc5235751
Sonu <sonulokre709@gmail.com>
2021-10-23 16:11:44 +0530

    initial commit

---
+++ music player/js/music-list.js
@@ -0,0 +1,35 @@
+
+let allMusic = [
+       {
+         name: "Legends never die",
+         artist: "Against The Current",
+         img: "legends never die",
+         src: "legends never die"
+       },
+       {
+         name: "Spectre",
+         artist: "alan-walker",
+         img: "spectre",
+         src: "spectre"
+       },
+       {
+         name: "Sia-Unstoppable",
+         artist: "Sia",
+         img: "sia unstoppable",
+         src: "sia unstoppable"
+       },
+       {
+         name: "On my way",
+         artist: "Alan-walker",
+         img: "on my way ",
+         src: "on my way"
+       },
+       {
+         name: "Darkside",
+         artist: "Alan-walker",
+         img: "darkside",
+         src: "darkside"
+       },
+      
+      
+     ];
